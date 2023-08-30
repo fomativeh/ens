b9d1b2ef77c5fd838151147353748fd23f16a5b3
Emmanuel Oyiboke <emmanueloyiboke@gmail.com>
2023-08-30 07:42:56 +0100

    migrated to app router
    migrated to app router
    
    fix

---
+++ frontend/app/admin/login/page.js
@@ -0,0 +1,12 @@
+import AdminLoginForm from "../components/login-form";
+import styles from "./style.module.css";
+
+export default function Login() {
+  return (
+    <>
+      <div>
+        <AdminLoginForm />
+      </div>
+    </>
+  );
+}
