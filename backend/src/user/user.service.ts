9225abc2b96febe07459818a61ddb4e2476ae9b8
Desmondezo1 <desezo@gmail.com>
2023-09-11 17:13:26 +0100

    created email service and returning user from refresh token

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -7,7 +7,6 @@ import {
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import internal from 'stream';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.schema';
