233c9650aae4b46b55f0b5aa7fa188d768dad955
fomativeh <ufomaitive@gmail.com>
2023-09-23 00:09:16 +0100

    SEO completed

--- frontend/components/SiteMeta.tsx
+++ frontend/components/SiteMeta.tsx
@@ -12,7 +12,7 @@ const SiteMeta = () => {
       <meta property="og:description" content="The Everything ENS Related." />
       <meta
         property="og:image"
-        content="https://m.media-amazon.com/images/I/71fhXpz0t2L._SY466_.jpg"
+        content="https://lh3.googleusercontent.com/pw/AIL4fc_zSdsdInkVuhwoEi4f9tIGN7CuUSuj_bocIcwkQ3sNcdbq98bk_EN429Id3QVLfS7P5Uu8lzGGOsxxKy3mGFOlKyD7nhr0ABTqeU0fdYvQNGJG89A-NXrYfbYf7npgaGkvVtNQNtHluoUVL3hOtXuTPXIIBZmGjIeXOkIyqusrVWjbDbD1mYDEOmYbD06LVUAHlRcxP0D-w6mcmu5PRMllzHc_R96GLweJypfePi9VhftKHNCA4Nr5RFrlhVAOHp_WU6VnSqQcKVn4a9wWM47hldyqpXK8d19Koj20bXkzD12bOg7aT8pMIZu1b76BqDNzvq7QEHsBkdclYylFvk7poOGNPlrWgGGFN9NraogzVZSqx0ufv1wL5lUR5lv4VKCi-r9T1GkzUL-yVHRgti01EoPWJViXX2f0wledVKPkfvIb5eI_HZx_w59aah9m8qNTAHSIRKxmHoFQg9LjDuTNi05Rui8LFMAj1Y6EkkVieTcvKadc_UoFE0YYFwaYRZMqyrrVpdLwiNigo7TDOXrz-8LsB58T1cRtFVtSoST9QW4JJje70XQmOkFVX2jgj0NnrFp75O9UxFDiGftQUdAMoBQJeVqK2nblQjtd-2dCoMsjtCnWWG-AMUaEZf10czW9echp2mEPv3EhgyEwA3NSgs_OJBttfuz1yB5wxvwkKnbODA3FHzQeQ1FrQ-Cd42mcXW98j1HFq60kfxf2WDOAQNMXdP_sjOxZJK-LGsBus_4dSvtfpTMCIpDofeq_V1QSiDW0VHrzPC-70FaTdtaM2X2GHWE4MtT12iygMbC8Yq5V73f-H1smuawJA5-p9I1TIvMCM6eiV4oSf3-8YBnrqxpXbrVC2MgtOjr9_DuDpKyRasxDKKqrmVtm-jnTpM7sJvzanmOZ-de6RAlx-VV2_rZKhAxxODtRSxN6vs3JPJjRfddGaSa6kubYRI638DZYobdM-SzjKU9GPxd3mWtwPA=w851-h884-s-no?authuser=0"
       />
       <meta property="og:url" content="https://ens-lite.vercel.app" />
       <meta property="og:type" content="website" />
@@ -23,7 +23,7 @@ const SiteMeta = () => {
       <meta name="twitter:description" content="The Everything ENS Related." />
       <meta
         name="twitter:image"
-        content="https://m.media-amazon.com/images/I/71fhXpz0t2L._SY466_.jpg"
+        content="https://lh3.googleusercontent.com/pw/AIL4fc_zSdsdInkVuhwoEi4f9tIGN7CuUSuj_bocIcwkQ3sNcdbq98bk_EN429Id3QVLfS7P5Uu8lzGGOsxxKy3mGFOlKyD7nhr0ABTqeU0fdYvQNGJG89A-NXrYfbYf7npgaGkvVtNQNtHluoUVL3hOtXuTPXIIBZmGjIeXOkIyqusrVWjbDbD1mYDEOmYbD06LVUAHlRcxP0D-w6mcmu5PRMllzHc_R96GLweJypfePi9VhftKHNCA4Nr5RFrlhVAOHp_WU6VnSqQcKVn4a9wWM47hldyqpXK8d19Koj20bXkzD12bOg7aT8pMIZu1b76BqDNzvq7QEHsBkdclYylFvk7poOGNPlrWgGGFN9NraogzVZSqx0ufv1wL5lUR5lv4VKCi-r9T1GkzUL-yVHRgti01EoPWJViXX2f0wledVKPkfvIb5eI_HZx_w59aah9m8qNTAHSIRKxmHoFQg9LjDuTNi05Rui8LFMAj1Y6EkkVieTcvKadc_UoFE0YYFwaYRZMqyrrVpdLwiNigo7TDOXrz-8LsB58T1cRtFVtSoST9QW4JJje70XQmOkFVX2jgj0NnrFp75O9UxFDiGftQUdAMoBQJeVqK2nblQjtd-2dCoMsjtCnWWG-AMUaEZf10czW9echp2mEPv3EhgyEwA3NSgs_OJBttfuz1yB5wxvwkKnbODA3FHzQeQ1FrQ-Cd42mcXW98j1HFq60kfxf2WDOAQNMXdP_sjOxZJK-LGsBus_4dSvtfpTMCIpDofeq_V1QSiDW0VHrzPC-70FaTdtaM2X2GHWE4MtT12iygMbC8Yq5V73f-H1smuawJA5-p9I1TIvMCM6eiV4oSf3-8YBnrqxpXbrVC2MgtOjr9_DuDpKyRasxDKKqrmVtm-jnTpM7sJvzanmOZ-de6RAlx-VV2_rZKhAxxODtRSxN6vs3JPJjRfddGaSa6kubYRI638DZYobdM-SzjKU9GPxd3mWtwPA=w851-h884-s-no?authuser=0"
       />
     </>
   );
