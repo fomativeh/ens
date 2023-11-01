1653709717b055d3a67bedbae6dd3373536a67f2
fomativeh <ufomaitive@gmail.com>
2023-11-01 14:23:20 +0100

    ...

--- frontend/components/SiteMeta.tsx
+++ frontend/components/SiteMeta.tsx
@@ -1,30 +1,28 @@
-// import SEOImg from "../public/assets/images/SEO.jpg"
-
 const SiteMeta = () => {
   return (
     <>
       <title>Domain Plug</title>
-      {/* <!-- Common Metadata --> */}
+      {/* Common Metadata */}
       <meta name="description" content="The Everything ENS Related." />
 
-      {/* <!-- Facebook, WhatsApp, Pinterest, LinkedIn --> */}
+      {/* Facebook, WhatsApp, Pinterest, LinkedIn */}
       <meta property="og:title" content="Domain Plug" />
       <meta property="og:description" content="The Everything ENS Related." />
-      <meta
-        property="og:image"
-        content="https://lh3.googleusercontent.com/pw/AIL4fc_zSdsdInkVuhwoEi4f9tIGN7CuUSuj_bocIcwkQ3sNcdbq98bk_EN429Id3QVLfS7P5Uu8lzGGOsxxKy3mGFOlKyD7nhr0ABTqeU0fdYvQNGJG89A-NXrYfbYf7npgaGkvVtNQNtHluoUVL3hOtXuTPXIIBZmGjIeXOkIyqusrVWjbDbD1mYDEOmYbD06LVUAHlRcxP0D-w6mcmu5PRMllzHc_R96GLweJypfePi9VhftKHNCA4Nr5RFrlhVAOHp_WU6VnSqQcKVn4a9wWM47hldyqpXK8d19Koj20bXkzD12bOg7aT8pMIZu1b76BqDNzvq7QEHsBkdclYylFvk7poOGNPlrWgGGFN9NraogzVZSqx0ufv1wL5lUR5lv4VKCi-r9T1GkzUL-yVHRgti01EoPWJViXX2f0wledVKPkfvIb5eI_HZx_w59aah9m8qNTAHSIRKxmHoFQg9LjDuTNi05Rui8LFMAj1Y6EkkVieTcvKadc_UoFE0YYFwaYRZMqyrrVpdLwiNigo7TDOXrz-8LsB58T1cRtFVtSoST9QW4JJje70XQmOkFVX2jgj0NnrFp75O9UxFDiGftQUdAMoBQJeVqK2nblQjtd-2dCoMsjtCnWWG-AMUaEZf10czW9echp2mEPv3EhgyEwA3NSgs_OJBttfuz1yB5wxvwkKnbODA3FHzQeQ1FrQ-Cd42mcXW98j1HFq60kfxf2WDOAQNMXdP_sjOxZJK-LGsBus_4dSvtfpTMCIpDofeq_V1QSiDW0VHrzPC-70FaTdtaM2X2GHWE4MtT12iygMbC8Yq5V73f-H1smuawJA5-p9I1TIvMCM6eiV4oSf3-8YBnrqxpXbrVC2MgtOjr9_DuDpKyRasxDKKqrmVtm-jnTpM7sJvzanmOZ-de6RAlx-VV2_rZKhAxxODtRSxN6vs3JPJjRfddGaSa6kubYRI638DZYobdM-SzjKU9GPxd3mWtwPA=w851-h884-s-no?authuser=0"
-      />
+      <meta property="og:image" content="/assets/images/SEO.jpg" />
       <meta property="og:url" content="https://ens-lite.vercel.app" />
       <meta property="og:type" content="website" />
 
-      {/* <!-- Twitter --> */}
+      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="Domain Plug" />
       <meta name="twitter:description" content="The Everything ENS Related." />
-      <meta
-        name="twitter:image"
-        content="https://lh3.googleusercontent.com/pw/AIL4fc_zSdsdInkVuhwoEi4f9tIGN7CuUSuj_bocIcwkQ3sNcdbq98bk_EN429Id3QVLfS7P5Uu8lzGGOsxxKy3mGFOlKyD7nhr0ABTqeU0fdYvQNGJG89A-NXrYfbYf7npgaGkvVtNQNtHluoUVL3hOtXuTPXIIBZmGjIeXOkIyqusrVWjbDbD1mYDEOmYbD06LVUAHlRcxP0D-w6mcmu5PRMllzHc_R96GLweJypfePi9VhftKHNCA4Nr5RFrlhVAOHp_WU6VnSqQcKVn4a9wWM47hldyqpXK8d19Koj20bXkzD12bOg7aT8pMIZu1b76BqDNzvq7QEHsBkdclYylFvk7poOGNPlrWgGGFN9NraogzVZSqx0ufv1wL5lUR5lv4VKCi-r9T1GkzUL-yVHRgti01EoPWJViXX2f0wledVKPkfvIb5eI_HZx_w59aah9m8qNTAHSIRKxmHoFQg9LjDuTNi05Rui8LFMAj1Y6EkkVieTcvKadc_UoFE0YYFwaYRZMqyrrVpdLwiNigo7TDOXrz-8LsB58T1cRtFVtSoST9QW4JJje70XQmOkFVX2jgj0NnrFp75O9UxFDiGftQUdAMoBQJeVqK2nblQjtd-2dCoMsjtCnWWG-AMUaEZf10czW9echp2mEPv3EhgyEwA3NSgs_OJBttfuz1yB5wxvwkKnbODA3FHzQeQ1FrQ-Cd42mcXW98j1HFq60kfxf2WDOAQNMXdP_sjOxZJK-LGsBus_4dSvtfpTMCIpDofeq_V1QSiDW0VHrzPC-70FaTdtaM2X2GHWE4MtT12iygMbC8Yq5V73f-H1smuawJA5-p9I1TIvMCM6eiV4oSf3-8YBnrqxpXbrVC2MgtOjr9_DuDpKyRasxDKKqrmVtm-jnTpM7sJvzanmOZ-de6RAlx-VV2_rZKhAxxODtRSxN6vs3JPJjRfddGaSa6kubYRI638DZYobdM-SzjKU9GPxd3mWtwPA=w851-h884-s-no?authuser=0"
-      />
+      <meta name="twitter:image" content="/assets/images/SEO.jpg" />
+
+      {/* WhatsApp-specific metadata */}
+      <meta property="og:site_name" content="Domain Plug" />
+
+      {/* Telegram-specific metadata */}
+      <meta name="telegram:site" content="Domain Plug" />
     </>
   );
 };
